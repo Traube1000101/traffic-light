cef0b99a04b6b719610fbd033ea3cc1eb7f38f16
Traube1000101 <girlscoutcookies420@gmx.net>
2024-07-22 17:33:54 +0200

    init

---
+++ src/main/java/org/traube/TrafficLight.java
@@ -0,0 +1,127 @@
+package org.traube;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.function.Consumer;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+class LightPanel extends JPanel {
+    private Color color;
+    private Boolean phase = false;
+
+    public LightPanel(Color color) {
+        this(color, false);
+    }
+
+    public LightPanel(Color color, Boolean defaultPhase) {
+        super();
+        this.color = color;
+        this.setBackground(this.color);
+        this.phase ^= defaultPhase;
+        this.toggle();
+    }
+
+    public void toggle() {
+        if (this.phase == true) {
+            this.setBackground(this.color);
+        } else {
+            this.setBackground(Color.black);
+        }
+        this.phase ^= true;
+    }
+}
+
+public class TrafficLight extends JFrame {
+    private String appTitle = "Traffic Light";
+    private Boolean phase = false;
+
+    private LightPanel greenLight, yellowLight, redLight;
+    private JButton button;
+
+    interface CompUtil extends Consumer<JComponent> {
+        static void setPanelSize(JComponent component) {
+            component.setPreferredSize(new Dimension(200, 200));
+        }
+    }
+
+    private <T extends JComponent> void addComponent(T component) {
+        this.addComponent(component, null);
+    }
+
+    private <T extends JComponent> void addComponent(T component, Consumer<T> function) {
+        component.setFont(new Font("FiraCode Nerd Font", Font.PLAIN, 32));
+        if (function != null)
+            function.accept(component);
+        this.add(component);
+    }
+
+    private void setTimeout(int delay, ActionListener listener) {
+        Timer timer = new Timer(delay, listener);
+        timer.setRepeats(false);
+        timer.start();
+    }
+
+    private StopWatch watch = new StopWatch();
+    private boolean notCalledYet = true;
+
+    private void switchLight(ActionEvent event) {
+        if (notCalledYet || (phase && watch.getTime() > 2000) || (!phase && watch.getTime() > 3000)) {
+            if (phase) {
+                greenLight.toggle();
+                yellowLight.toggle();
+                setTimeout(3000, e -> {
+                    yellowLight.toggle();
+                    redLight.toggle();
+                });
+
+            } else {
+                yellowLight.toggle();
+                setTimeout(2000, e1 -> {
+                    redLight.toggle();
+                    yellowLight.toggle();
+                    greenLight.toggle();
+                });
+            }
+            phase ^= true;
+            notCalledYet = false;
+            watch.reset();
+            watch.start();
+        }
+    }
+
+    public TrafficLight() {
+        this.setTitle(appTitle);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLayout(new GridLayout(4, 1, 10, 10));
+
+        System.setProperty("awt.useSystemAAFontSettings", "on"); // For antialiasing text
+
+        redLight = new LightPanel(Color.red, true);
+        yellowLight = new LightPanel(Color.yellow);
+        greenLight = new LightPanel(Color.green);
+        button = new JButton("Switch");
+
+        addComponent(redLight, CompUtil::setPanelSize);
+        addComponent(yellowLight, CompUtil::setPanelSize);
+        addComponent(greenLight, CompUtil::setPanelSize);
+        addComponent(button, element -> element.addActionListener(this::switchLight));
+
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        new TrafficLight();
+    }
+}
