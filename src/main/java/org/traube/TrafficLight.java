5b3d5367745818a5a4f5bda618589978f8518b88
Traube1000101 <girlscoutcookies420@gmx.net>
2024-07-23 20:18:24 +0200

    feat: add antialiasing toggle

--- src/main/java/org/traube/TrafficLight.java
+++ src/main/java/org/traube/TrafficLight.java
@@ -1,6 +1,7 @@
 package org.traube;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -9,12 +10,18 @@ import java.awt.GridLayout;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.border.Border;
@@ -24,6 +31,7 @@ import org.apache.commons.lang3.time.StopWatch;
 class LightPanel extends JPanel {
     private Color color;
     private Boolean phase = false;
+    private static boolean useAntialiasing = true;
 
     public LightPanel(Color color) {
         this(color, false);
@@ -36,6 +44,10 @@ class LightPanel extends JPanel {
         this.setOpaque(false);
         this.phase ^= defaultPhase;
         this.toggle();
+    }
+
+    public static void setAntialiasing(boolean useAntialiasing) {
+        LightPanel.useAntialiasing = useAntialiasing;
     }
 
     public void toggle() {
@@ -51,7 +63,9 @@ class LightPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (useAntialiasing == true) {
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        }
         int width = g2d.getClipBounds().width, height = g2d.getClipBounds().height;
         g2d.setColor(new Color(48, 48, 48));
         g2d.fillOval(0, 0, width - 1, height - 1);
@@ -61,12 +75,40 @@ class LightPanel extends JPanel {
     }
 }
 
+class ComboboxToolTipRenderer extends DefaultListCellRenderer {
+    List<String> tooltips;
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean cellHasFocus) {
+
+        if (-1 < index && null != value && null != tooltips) {
+            list.setToolTipText(tooltips.get(index));
+        }
+
+        JLabel label = (JLabel) super.getListCellRendererComponent(list,
+                value, index, isSelected, cellHasFocus);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        return label;
+    }
+
+    public void setTooltips(List<String> tooltips) {
+        this.tooltips = tooltips;
+    }
+}
+
 public class TrafficLight extends JFrame {
     private String appTitle = "Traffic Light";
     private Boolean phase = false;
+    private short fontSize = 20;
 
     private LightPanel greenLight, yellowLight, redLight;
+    private JPanel controlPanel;
+    private JPanel comboBoxPanel;
+    private JComboBox<String> comboBox;
     private JButton button;
+    private JLabel comboBoxLabel;
 
     interface CompUtil extends Consumer<JComponent> {
         static void setPanelSize(JComponent component) {
@@ -79,7 +121,6 @@ public class TrafficLight extends JFrame {
     }
 
     private <T extends JComponent> void addComponent(T component, Consumer<T> function) {
-        component.setFont(new Font("FiraCode Nerd Font", Font.PLAIN, 32));
         if (function != null)
             function.accept(component);
         this.add(component);
@@ -97,6 +138,7 @@ public class TrafficLight extends JFrame {
     private void switchLight(ActionEvent event) {
         if (notCalledYet || (phase && watch.getTime() > 2000) || (!phase &&
                 watch.getTime() > 3000)) {
+            LightPanel.setAntialiasing(comboBox.getSelectedIndex() == 0);
             if (phase) {
                 greenLight.toggle();
                 yellowLight.toggle();
@@ -136,12 +178,38 @@ public class TrafficLight extends JFrame {
         redLight = new LightPanel(Color.red, true);
         yellowLight = new LightPanel(Color.yellow);
         greenLight = new LightPanel(Color.green);
-        button = new JButton("Switch");
+
+        List<String> tooltips = new ArrayList();
+        comboBox = new JComboBox<>();
+        comboBox.setFont(new Font("FiraCode Nerd Font", Font.PLAIN, fontSize));
+        ComboboxToolTipRenderer renderer = new ComboboxToolTipRenderer();
+        comboBox.setRenderer(renderer);
+        comboBox.addItem("Enable");
+        tooltips.add("Sometimes delays light switching");
+        comboBox.addItem("Disable");
+        tooltips.add("Fast rendering but jagged edges");
+        renderer.setTooltips(tooltips);
+        comboBoxLabel = new JLabel("Antialiasing");
+        comboBoxLabel.setFont(new Font("FiraCode Nerd Font", Font.PLAIN, fontSize));
+        comboBoxLabel.setHorizontalAlignment(JLabel.CENTER);
+        comboBoxPanel = new JPanel(new GridLayout(2, 1, 0, 0));
+        comboBoxPanel.add(comboBoxLabel);
+        comboBoxPanel.add(comboBox);
+
+        button = new JButton("Change");
+        button.setFont(new Font("FiraCode Nerd Font", Font.PLAIN, fontSize));
+        button.addActionListener(this::switchLight);
+
+        controlPanel = new JPanel(new GridLayout(2, 1, 10, 10));
+        controlPanel.setPreferredSize(new Dimension(200, 200));
+        controlPanel.setOpaque(false);
+        controlPanel.add(comboBoxPanel);
+        controlPanel.add(button);
 
         addComponent(redLight, CompUtil::setPanelSize);
         addComponent(yellowLight, CompUtil::setPanelSize);
         addComponent(greenLight, CompUtil::setPanelSize);
-        addComponent(button, element -> element.addActionListener(this::switchLight));
+        addComponent(controlPanel);
 
         this.pack();
         this.setVisible(true);
